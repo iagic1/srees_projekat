5001c79ef6698cd7cdd279df867a1c64a71306da
iagic1 <116057266+iagic1@users.noreply.github.com>
2024-04-27 12:10:35 +0200

    Add files via upload

---
+++ SREES/src/Application.h
@@ -0,0 +1,40 @@
+//
+//  Created by Izudin Dzafic on 28/07/2020.
+//  Copyright © 2020 IDz. All rights reserved.
+//
+#pragma once
+#include <gui/Application.h>
+#include "MainWindow.h"
+#include <dp/IDatabase.h>
+
+class Application : public gui::Application
+{
+protected:
+    
+    gui::Window* createInitialWindow() override
+    {
+        //setInitialFrameSize(gui::Window::FrameSize::Maximized);
+        return new MainWindow();
+    }
+    
+public:
+    Application(int argc, const char** argv)
+    : gui::Application(argc, argv)
+    {
+    }
+
+    bool connectToDB()
+    {
+       // dp::IDatabase* pDB = dp::create(dp::IDatabase::ConnType::CT_SQLITE, dp::IDatabase::ServerType::SER_SQLITE3);
+
+      //  fo::fs::path home;
+     //   mu::getHomePath(home);
+
+      //  fo::fs::path DBPath = (home / "Work/PZI_Baza/baza.db");
+       // if (!pDB->connect(DBPath.string().c_str()))
+        //    return false;
+
+      //  dp::setMainDatabase(pDB);
+        return true;
+    }
+};
---
+++ SREES/src/MainView.h
@@ -0,0 +1,65 @@
+//
+//  Created by Izudin Dzafic on 28/07/2020.
+//  Copyright © 2020 IDz. All rights reserved.
+//
+#pragma once
+#include <gui/Controls.h>
+#include <gui/Navigator.h>
+#include "SwitcherView.h"
+
+class MainView: public gui::View
+{
+private:
+protected:
+    gui::HorizontalLayout _hl;
+    gui::Navigator _navigator;
+    gui::Image _navImg1;
+    gui::Image _navImg2;
+    gui::Symbol _navSymb3;
+    gui::Image _navImg4;
+    gui::Image _navImg5;
+    SwitcherView _switcherView;
+public:
+    MainView()
+    : _hl(2)
+    , _navigator(5, true)
+    , _navImg1(":grid")
+    , _navImg2(":canvas")
+    , _navSymb3(":Xfmr4")
+    , _navImg4(":connOK")
+    , _navImg5(":settings")
+    {
+        setMargins(0,0,0,0);
+        _navigator.setItem(0, &_navImg1, "O nama");
+        _navigator.setItem(1, &_navImg2, "Kablovi");
+        _navigator.setItem(3, &_navSymb3, "Transformatori");
+        _navigator.setItem(2, &_navImg4, "Nadzemni vodovi");
+        _navigator.setItem(4, &_navImg5, "Horizontal Grid Demo");
+        _hl.append(_navigator);
+        
+        //_imgView.setImage(_navImg1);
+        _hl.append(_switcherView);
+        setLayout(&_hl);
+    }
+protected:
+    bool onActionItem(td::BYTE menuID, td::BYTE firstSubMenuID, td::BYTE lastSubMenuID, td::BYTE actionID, gui::ActionItem* pAI) override
+    {
+        td::String msgText("Handling onActionItem");
+        td::String informativeText;
+        informativeText.format("Handled onActionItem(subMenuID=%d, firstSubSubMenuID=%d, lastSubSubMenuID=%d, actionID=%d)", menuID, firstSubMenuID, lastSubMenuID, actionID);
+        
+       showAlert(msgText, informativeText);
+        return false;
+    }
+    
+    
+    bool onChangedSelection(gui::Navigator* pNav) override
+    {
+        td::UINT2 currSelection = pNav->getCurrentSelection();
+        _switcherView.showView((int) currSelection);
+        //mu::dbgLog("Navigator selChanged. New Sel =%d", currSelection);
+        return true;
+    }
+    
+};
+
